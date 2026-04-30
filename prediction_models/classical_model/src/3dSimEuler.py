885ee39e04849e1cf79cc5e8ea60b7593d7f33c1
pawel <pawel.leczkowski2005@gmail.com>
2026-04-30 15:07:40 +0200

    euler and rk4 implemented

---
+++ prediction_models/classical_model/src/3dSimEuler.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import constants as const
+
+def CalculateNonInertialForces(velocity, position):
+    earth_angular_velocity = 7.2921159e-5
+    earth_angular_velocity_vector = np.array([0, 0, earth_angular_velocity])
+    coriolis = -2 * np.cross(earth_angular_velocity_vector, velocity)
+    centrifugal = -np.cross(earth_angular_velocity_vector, np.cross(earth_angular_velocity_vector, position))
+    return coriolis, centrifugal
+
+def Newton(dt, fuel_mass, rocket_mass, thrust, position, velocity):
+    earth_mass = 5.972e24
+    gravity = -const.G * earth_mass * position / max(np.linalg.norm(position) ** 3, 1e-8)
+    coriolis, centrifugal = CalculateNonInertialForces(velocity, position)
+
+    mass = rocket_mass + fuel_mass
+    new_acceleration = thrust / max(mass, 1e-8) + gravity + coriolis + centrifugal
+
+    new_velocity = velocity + new_acceleration * dt
+    new_position = position + new_velocity * dt
+
+    return new_position, new_velocity, new_acceleration
--- prediction_models/classical_model/src/3dSimRK4.py
+++ prediction_models/classical_model/src/3dSimRK4.py
@@ -1,91 +1,41 @@
-import math
 import numpy as np
-from matplotlib import pyplot as plt
 from scipy import constants as const
-import isacalc as isa
-import pandas as pd
-from SimHelper import *
-
-# ---------------------------- Symulacja ----------------------------
-
-def CalculateThrust(position):
-    # todo ciag zmienia sie wraz z cisnieniem
-    up = position / max(np.linalg.norm(position), 1e-8)
-    east = np.cross(np.array([0, 0, 1]), up)
-
-    norm = np.linalg.norm(east)
-    if norm < 1e-8:
-        east = np.array([1,0,0])
-    else:
-        east /= norm
-
-    north = np.cross(up, east)
-    north /= max(np.linalg.norm(north), 1e-8)
-    thrust_dir = (
-            math.cos(pitch) * up +
-            math.sin(pitch) * north
-    )
-    thrust_vector = thrust * thrust_dir
-    return thrust_vector
 
 def CalculateNonInertialForces(velocity, position):
+    earth_angular_velocity = 7.2921159e-5
+    earth_angular_velocity_vector = np.array([0, 0, earth_angular_velocity])
     coriolis = -2 * np.cross(earth_angular_velocity_vector, velocity)
     centrifugal = -np.cross(earth_angular_velocity_vector, np.cross(earth_angular_velocity_vector, position))
     return coriolis, centrifugal
 
-def RK4(dt, fuel_mass, flight_time):
-    def acceleration(position, velocity, fuelMass, flightTime):
-        if flightTime < burn_time and fuelMass > 0:
-            thrust_vector = CalculateThrust(position)
-        else:
-            thrust_vector = np.zeros(3)
-
+def RK4(dt, fuel_mass, rocket_mass, thrust, position, velocity):
+    def acceleration(position, velocity, fuelMass):
+        earth_mass = 5.972e24
         gravity = -const.G * earth_mass * position / max(np.linalg.norm(position) ** 3, 1e-8)
         coriolis, centrifugal = CalculateNonInertialForces(velocity, position)
 
         mass = rocket_mass + fuelMass
-        return (thrust_vector) / max(mass, 1e-8) + gravity + coriolis + centrifugal, gravity
+        acc = thrust / max(mass, 1e-8) + gravity + coriolis + centrifugal
+        return acc
 
-    k1_acceleration, k1_gravity = acceleration(position_global, velocity_global, max(fuel_mass - fuel_consumption * dt / 4, 0), flight_time)
+    k1_acceleration = acceleration(position, velocity, fuel_mass)
     k1_velocity = k1_acceleration * dt
-    k1_position = velocity_global * dt
+    k1_position = velocity * dt
 
-    k2_acceleration, k2_gravity = acceleration(position_global + 0.5 * k1_position, velocity_global + 0.5 * k1_velocity, max(fuel_mass - fuel_consumption * dt / 2, 0), flight_time + 0.5 * dt)
+    k2_acceleration = acceleration(position + 0.5 * k1_position, velocity + 0.5 * k1_velocity, fuel_mass)
     k2_velocity = k2_acceleration * dt
-    k2_position = (velocity_global + 0.5 * k1_velocity) * dt
+    k2_position = (velocity + 0.5 * k1_velocity) * dt
 
-    k3_acceleration, k3_gravity = acceleration(position_global + 0.5 * k2_position, velocity_global + 0.5 * k2_velocity, max(fuel_mass - fuel_consumption * dt * 3 / 4, 0), flight_time + 0.5 * dt)
+    k3_acceleration = acceleration(position + 0.5 * k2_position, velocity + 0.5 * k2_velocity, fuel_mass)
     k3_velocity = k3_acceleration * dt
-    k3_position = (velocity_global + 0.5 * k2_velocity) * dt
+    k3_position = (velocity + 0.5 * k2_velocity) * dt
 
-    k4_acceleration, k4_gravity = acceleration(position_global + k3_position, velocity_global + k3_velocity, max(fuel_mass - fuel_consumption * dt, 0), flight_time + dt)
+    k4_acceleration = acceleration(position + k3_position, velocity + k3_velocity, fuel_mass)
     k4_velocity = k4_acceleration * dt
-    k4_position = (velocity_global + k3_velocity) * dt
+    k4_position = (velocity + k3_velocity) * dt
 
     new_acceleration = (k1_acceleration + 2 * k2_acceleration + 2 * k3_acceleration + k4_acceleration) / 6
-    new_velocity = velocity_global + (k1_velocity + 2 * k2_velocity + 2 * k3_velocity + k4_velocity) / 6
-    new_position = position_global + (k1_position + 2 * k2_position + 2 * k3_position + k4_position) / 6
-    new_gravity = (k1_gravity + 2 * k2_gravity + 2 * k3_gravity + k4_gravity) / 6
+    new_velocity = velocity + (k1_velocity + 2 * k2_velocity + 2 * k3_velocity + k4_velocity) / 6
+    new_position = position + (k1_position + 2 * k2_position + 2 * k3_position + k4_position) / 6
 
-    return new_position, new_velocity, new_acceleration, new_gravity
-
-while True:
-    height = np.linalg.norm(position_global) - earth_radius
-    if height <= 0 and flight_time > 0:
-        break
-
-    pitch += angle_rate * dt
-
-    if flight_time < burn_time:
-        fuel_mass -= fuel_consumption * dt
-        fuel_mass = max(fuel_mass, 0)
-    elif thrust != 0:
-        thrust = 0
-        free_flight_Y = position_global[2]
-        angle_rate = 0
-
-    position_global, velocity_global, acceleration_local, gravity_local = RK4(dt, fuel_mass, flight_time)
-
-    dynamic_pressure = atm.calculate(h=height)[2] * np.linalg.norm(velocity_global) ** 2 / 2
-
-    flight_time += dt
+    return new_position, new_velocity, new_acceleration
